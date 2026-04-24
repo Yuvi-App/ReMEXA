7357a93cbf217a428f9a54e586fcaab1806ad227
Yuvi-App <admin@yuvi.app>
2026-04-24 19:47:33 -0400

    RMS AppManager update

--- src/main/java/com/j_phone/system/ApplicationManager.java
+++ src/main/java/com/j_phone/system/ApplicationManager.java
@@ -2,37 +2,76 @@ package com.j_phone.system;
 
 public class ApplicationManager {
     public static final int F_MENU = 0;
-    public static final int J_SKY_MENU = 0;
-    public static final int MAIL_MENU = 0;
-    public static final int WEB_MENU = 0;
+    public static final int J_SKY_MENU = 1;
+    public static final int MAIL_MENU = 2;
+    public static final int WEB_MENU = 3;
+
+    private static final com.j_phone.system.ApplicationManager INSTANCE = new com.j_phone.system.ApplicationManager();
+
+    private int pausedTransitMenu = F_MENU;
+    private long wakeupDeadlineMillis;
+    private com.j_phone.system.WakeupListener wakeupListener;
+
+    private ApplicationManager() {
+    }
 
     public static com.j_phone.system.ApplicationManager getInstance () {
         remexa.probes.SdkStubSupport.log("com.j_phone.system.ApplicationManager", "getInstance");
-        return null;
+        return INSTANCE;
     }
 
     public void setPausedTransitMenu (int type) throws java.lang.IllegalArgumentException, java.lang.RuntimeException {
+        validatePausedTransitMenu(type);
+        this.pausedTransitMenu = type;
         remexa.probes.SdkStubSupport.log("com.j_phone.system.ApplicationManager", "setPausedTransitMenu", type);
     }
 
     public void scheduleWakeup (int time) throws java.lang.IllegalArgumentException, java.lang.RuntimeException {
+        if (time < 0) {
+            throw new IllegalArgumentException("time must be >= 0");
+        }
+        this.wakeupDeadlineMillis = System.currentTimeMillis() + (time * 1000L);
         remexa.probes.SdkStubSupport.log("com.j_phone.system.ApplicationManager", "scheduleWakeup", time);
     }
 
     public void cancelWakeup () throws java.lang.RuntimeException {
+        this.wakeupDeadlineMillis = 0L;
         remexa.probes.SdkStubSupport.log("com.j_phone.system.ApplicationManager", "cancelWakeup");
     }
 
     public int getWakeup () throws java.lang.RuntimeException {
         remexa.probes.SdkStubSupport.log("com.j_phone.system.ApplicationManager", "getWakeup");
-        return 0;
+        if (wakeupDeadlineMillis <= 0L) {
+            return 0;
+        }
+        long remainingMillis = wakeupDeadlineMillis - System.currentTimeMillis();
+        if (remainingMillis <= 0L) {
+            wakeupDeadlineMillis = 0L;
+            return 0;
+        }
+        return (int) ((remainingMillis + 999L) / 1000L);
     }
 
     public void setWakeupListener (com.j_phone.system.WakeupListener listener) {
+        this.wakeupListener = listener;
         remexa.probes.SdkStubSupport.log("com.j_phone.system.ApplicationManager", "setWakeupListener", listener);
     }
 
     public void flushRMS () {
         remexa.probes.SdkStubSupport.log("com.j_phone.system.ApplicationManager", "flushRMS");
+    }
+
+    public int pausedTransitMenu() {
+        return pausedTransitMenu;
+    }
+
+    public com.j_phone.system.WakeupListener wakeupListener() {
+        return wakeupListener;
+    }
+
+    private static void validatePausedTransitMenu(int type) {
+        if (type != F_MENU && type != J_SKY_MENU && type != MAIL_MENU && type != WEB_MENU) {
+            throw new IllegalArgumentException("Unsupported paused transit menu: " + type);
+        }
     }
 }
