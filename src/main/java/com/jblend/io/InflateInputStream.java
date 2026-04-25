bec928a72c8042713b96b12ae77a8385b73995e7
Yuvi-App <admin@yuvi.app>
2026-04-24 20:46:53 -0400

    InflateStream apis implemented

--- src/main/java/com/jblend/io/InflateInputStream.java
+++ src/main/java/com/jblend/io/InflateInputStream.java
@@ -1,31 +1,62 @@
 package com.jblend.io;
 
-public final class InflateInputStream extends java.io.InputStream {
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+public final class InflateInputStream extends InputStream {
+    private final InputStream delegate;
+
     protected InflateInputStream() {
-        remexa.probes.SdkStubSupport.log("com.jblend.io.InflateInputStream", "InflateInputStream");
+        this.delegate = InputStream.nullInputStream();
     }
 
-    public InflateInputStream (java.io.InputStream in) {
-        remexa.probes.SdkStubSupport.log("com.jblend.io.InflateInputStream", "InflateInputStream", in);
+    public InflateInputStream(InputStream in) throws IOException {
+        this.delegate = createInflaterStream(in);
     }
 
+    private static InputStream createInflaterStream(InputStream in) throws IOException {
+        PushbackInputStream probe = new PushbackInputStream(new BufferedInputStream(in), 2);
+        byte[] header = new byte[2];
+        int count = probe.read(header);
+        if (count > 0) {
+            probe.unread(header, 0, count);
+        }
 
-    public int read () throws java.io.IOException {
-        remexa.probes.SdkStubSupport.log("com.jblend.io.InflateInputStream", "read");
-        return 0;
+        boolean nowrap = count == 2 && !looksLikeZlibHeader(header[0] & 0xFF, header[1] & 0xFF);
+        return new InflaterInputStream(probe, new Inflater(nowrap));
     }
 
-    public int read (byte[] b, int off, int len) throws java.io.IOException {
-        remexa.probes.SdkStubSupport.log("com.jblend.io.InflateInputStream", "read", b, off, len);
-        return 0;
+    private static boolean looksLikeZlibHeader(int cmf, int flg) {
+        if ((cmf & 0x0F) != 8) {
+            return false;
+        }
+        if (((cmf << 8) | flg) % 31 != 0) {
+            return false;
+        }
+        return (cmf >> 4) <= 7;
     }
 
-    public int read (byte[] b) throws java.io.IOException {
-        remexa.probes.SdkStubSupport.log("com.jblend.io.InflateInputStream", "read", b);
-        return 0;
+    @Override
+    public int read() throws IOException {
+        return delegate.read();
     }
 
-    public void close () throws java.io.IOException {
-        remexa.probes.SdkStubSupport.log("com.jblend.io.InflateInputStream", "close");
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return delegate.read(b, off, len);
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+        return delegate.read(b);
+    }
+
+    @Override
+    public void close() throws IOException {
+        delegate.close();
     }
 }
