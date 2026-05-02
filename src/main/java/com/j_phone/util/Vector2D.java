0ecc5679e52ec478efcc48c72c5a611e491ca87f
Yuvi-App <admin@yuvi.app>
2026-05-02 19:42:44 -0400

    Implemented missing APIs for Vector2D causing ys crash

--- src/main/java/com/j_phone/util/Vector2D.java
+++ src/main/java/com/j_phone/util/Vector2D.java
@@ -1,69 +1,122 @@
 package com.j_phone.util;
 
 public class Vector2D {
-    public Vector2D (com.j_phone.util.FixedPoint x, com.j_phone.util.FixedPoint y) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "Vector2D", x, y);
+    private static final int FRACTION_BITS = 16;
+    private static final int ONE = 1 << FRACTION_BITS;
+
+    private int x;
+    private int y;
+
+    public Vector2D(FixedPoint x, FixedPoint y) {
+        this.x = x == null ? 0 : encode(x);
+        this.y = y == null ? 0 : encode(y);
     }
 
-    public Vector2D (int x, int y) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "Vector2D", x, y);
+    public Vector2D(int x, int y) {
+        this.x = x << FRACTION_BITS;
+        this.y = y << FRACTION_BITS;
     }
 
-    public Vector2D () {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "Vector2D");
+    public Vector2D() {
+        this(0, 0);
     }
 
-
-    public void add (com.j_phone.util.Vector2D vector) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "add", vector);
+    public void add(Vector2D vector) {
+        if (vector == null) {
+            return;
+        }
+        x += vector.x;
+        y += vector.y;
     }
 
-    public void add (int x, int y) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "add", x, y);
+    public void add(int x, int y) {
+        this.x += x << FRACTION_BITS;
+        this.y += y << FRACTION_BITS;
     }
 
-    public void subtract (com.j_phone.util.Vector2D vector) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "subtract", vector);
+    public void subtract(Vector2D vector) {
+        if (vector == null) {
+            return;
+        }
+        x -= vector.x;
+        y -= vector.y;
     }
 
-    public void subtract (int x, int y) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "subtract", x, y);
+    public void subtract(int x, int y) {
+        this.x -= x << FRACTION_BITS;
+        this.y -= y << FRACTION_BITS;
     }
 
-    public void normalize () {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "normalize");
+    public void normalize() {
+        if (x == 0 && y == 0) {
+            return;
+        }
+
+        double xd = x / (double) ONE;
+        double yd = y / (double) ONE;
+        double length = Math.hypot(xd, yd);
+        if (length == 0.0d) {
+            x = 0;
+            y = 0;
+            return;
+        }
+
+        x = (int) Math.round((xd / length) * ONE);
+        y = (int) Math.round((yd / length) * ONE);
     }
 
-    public static com.j_phone.util.FixedPoint innerProduct (com.j_phone.util.Vector2D v1, com.j_phone.util.Vector2D v2) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "innerProduct", v1, v2);
-        return null;
+    public static FixedPoint innerProduct(Vector2D v1, Vector2D v2) {
+        if (v1 == null || v2 == null) {
+            return new FixedPoint(0);
+        }
+        long raw = ((long) v1.x * v2.x + (long) v1.y * v2.y) >> FRACTION_BITS;
+        return new FixedPoint(saturate(raw));
     }
 
-    public static com.j_phone.util.FixedPoint outerProduct (com.j_phone.util.Vector2D v1, com.j_phone.util.Vector2D v2) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "outerProduct", v1, v2);
-        return null;
+    public static FixedPoint outerProduct(Vector2D v1, Vector2D v2) {
+        if (v1 == null || v2 == null) {
+            return new FixedPoint(0);
+        }
+        long raw = ((long) v1.x * v2.y - (long) v1.y * v2.x) >> FRACTION_BITS;
+        return new FixedPoint(saturate(raw));
     }
 
-    public void setValue (int x, int y) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "setValue", x, y);
+    public void setValue(int x, int y) {
+        this.x = x << FRACTION_BITS;
+        this.y = y << FRACTION_BITS;
     }
 
-    public void setValue (com.j_phone.util.FixedPoint x, com.j_phone.util.FixedPoint y) {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "setValue", x, y);
+    public void setValue(FixedPoint x, FixedPoint y) {
+        this.x = x == null ? 0 : encode(x);
+        this.y = y == null ? 0 : encode(y);
     }
 
-    public com.j_phone.util.FixedPoint getX () {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "getX");
-        return null;
+    public FixedPoint getX() {
+        return new FixedPoint(x);
     }
 
-    public com.j_phone.util.FixedPoint getY () {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "getY");
-        return null;
+    public FixedPoint getY() {
+        return new FixedPoint(y);
     }
 
-    public com.j_phone.util.Vector2D clone () {
-        remexa.probes.SdkStubSupport.log("com.j_phone.util.Vector2D", "clone");
-        return null;
+    public Vector2D clone() {
+        Vector2D clone = new Vector2D();
+        clone.x = x;
+        clone.y = y;
+        return clone;
+    }
+
+    private static int encode(FixedPoint value) {
+        return (value.getInteger() << FRACTION_BITS) + value.getDecimal();
+    }
+
+    private static int saturate(long raw) {
+        if (raw > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (raw < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int) raw;
     }
 }
